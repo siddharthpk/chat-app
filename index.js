fd042093a0a61e00fa87aa9940b7852035cb5c31
Siddharth Pathak <siddharthpathak415@gmail.com>
2019-11-23 15:04:15 -0800

    Final updates

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-/*
-This javascript file uses express application web framework to service
-web requests. It also uses socket.io for real time synchronized 
-communication
-*/
 
-
-var express = require('express');
-var socket = require('socket.io');
+const express = require('express');
+const socket = require('socket.io');
 
 const PORT = process.env.PORT||3000;
 
 // App setup
 var app = express();
 const server = app.listen(PORT, function(){
-    console.log('listening for requests');
+    console.log('Connection Successful! Listening at Port 3000');
 });
 
 // Static files
@@ -24,10 +18,11 @@ const io = socket(server);
 io.on('connection', (socket) => {
 
     console.log('made socket connection', socket.id);
-    
+    //socket.on('disconnect',() => console.log('Client disconnected'));
+
     // Handle chat event
     socket.on('chat', function(data){
-        
+        // console.log(data);
         io.sockets.emit('chat', data);
     });
 
